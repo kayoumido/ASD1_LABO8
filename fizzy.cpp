d92654f8061cb348c5f7b625e379baf54da55dc3
Loïc Dessaules <loic.dessaules@gmail.com>
2019-05-13 19:35:02 +0200

    WIP

--- fizzy.cpp
+++ fizzy.cpp
@@ -10,22 +10,19 @@ using std::forward_list;
 void fizzBuzz(std::forward_list<int>& L,
               std::forward_list<int>& F,
               std::forward_list<int>& B,
-              std::forward_list<int>& FB)
-{
-  assert(F.empty() and B.empty() and FB.empty());
-  for (auto it = L.begin(); it != L.end(); ++it) {
-    if ((*it % 3) == 0 && (*it % 5) == 0) {
-      FB.splice_after(FB.end(), L, it);
-    } else if ((*it % 3) == 0 && (*it % 5) != 0) {
-      F.splice_after(F.end(), L, it);
-      std::cout << *it << '\n';
+              std::forward_list<int>& FB) {
+    assert(F.empty() and B.empty() and FB.empty());
+
+    
+    for (auto it = L.begin(), beforeIt = L.before_begin(); it != L.end(); ++it, ++beforeIt) {
+        if ((*it % 3) == 0 && (*it % 5) == 0) {
+            FB.splice_after(FB.before_begin(), L, beforeIt);
+        } else if ((*it % 3) == 0) {
+            F.splice_after(F.before_begin(), L, beforeIt);
+        } else if ((*it % 5) == 0) {
+            B.splice_after(B.before_begin(), L, beforeIt);
+        }
     }
-    // else if ((*it % 3) != 0 && (*it % 5) == 0) {
-    //   F.splice_after(F.before_begin(), B, B.before_begin());
-    // }
-    // else if ((*it % 3) == 0 && (*it % 5) == 0) {
-    //   F.splice_after(F.before_begin(), FB, FB.before_begin());
-    // }
-  }
-// à compléter
+
+
 }
