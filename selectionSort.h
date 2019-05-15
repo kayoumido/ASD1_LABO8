200653a3587f0904df09a02534d43434deb4a3d8
Doran Kayoumi <dorankayoumi@gmail.com>
2019-05-15 19:02:27 +0200

    Implement selectsort for forward list

--- selectionSort.h
+++ selectionSort.h
@@ -7,8 +7,30 @@ template <class T>
 void selectionSort(std::forward_list<T>& L) {
     using namespace std;
 
-//  Complétez cette fonction
+    if (L.empty()) return;
 
+    auto j = L.before_begin();
+
+    while (next(j, 2) != L.end()) {
+
+        auto imin   = j;
+        auto i      = next(j);
+
+        // search for the min
+        while (next(i) != L.end()) {
+            // since we're sorting a forward_list using splice_after,
+            // we need to check if the NEXT element is the min.
+            // this is because splice_after moves the element after the iterator it's given
+            if (*next(i) < *next(imin))
+                imin = i;
+
+            i = next(i);
+        }
+
+        L.splice_after(j, L, imin);
+        std::cout << L << std::endl;
+        j = next(j);
+    }
 }
 
 #endif
